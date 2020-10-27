efc5b7974e00003d473741eb377990fc7aeb97ec
brunoFernandes <bwendel26999@gmail.com>
2020-10-27 01:26:07 -0300

    starting the backends config files

--- versao-inicial/backend/index.js
+++ versao-inicial/backend/index.js
@@ -1,4 +1,12 @@
-const app = require('express')()
+const app = require('express')();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+// const consign = require('consign');
+
+// consign()
+//     .then('./config/middlewares.js')
+//     .then(app)
 
 app.listen(3000, () => {
     console.log('Backend executando...');
